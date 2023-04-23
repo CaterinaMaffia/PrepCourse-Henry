e36dd9014b184d31405b1c5d32ed1e8ca9a38367
CaterinaMaffia <ccaterinammaffia@gmail.com>
2023-04-23 11:00:51 -0300

    funciones de java script

---
+++ funciones2.js
@@ -0,0 +1,13 @@
+function cuidadoConElConsoleLog(nombre){
+    console.log(nombre);
+}
+function otraFuncion() {
+    return (
+        "El nombre retornado por la funcion 'cuidadoConElConsoleLog' es: " + cuidadoConElConsoleLog("Camilo")
+    );
+}
+
+function cuidadoConElReturn(nombre) {
+    return nombre;
+    console.log(nombre);
+}
